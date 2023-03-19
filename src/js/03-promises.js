b554ed3920a5fd363e66d74ce2e7a6f202817f76
Lisa Riabokon <lisa.checheleva@gmail.com>
2023-03-19 13:28:03 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,33 @@
+//установка бібліотеки $ npm i notiflix
+import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
+
+const form = {
+  formEl: document.querySelector('.form'),
+  delayEl: document.querySelector('input[name="delay"]'),
+  stepEl: document.querySelector('input[name="step"]'),
+  amountEl: document.querySelector('input[name="amount"]'),
+  buttonEl: document.querySelector('button[type="submit"]'),
+};
+
+form.buttonEl.addEventListener('click', onCreatePromise);
+
+function onCreatePromise(evt) {
+  // скасовуємо перезавантаження за замовчуванням
+  evt.preventDefault();
+
+  //збір даних з input та конветрація в числа
+  let delayValue = Number(form.delayEl.value);
+  let stepValue = Number(form.stepEl.value);
+  let amountValue = Number(form.amountEl.value);
+
+  // console.log(delayValue);
+  // console.log(stepValue);
+  // console.log(amountValue);
+
+  for (let i = 1; i <= amountValue; i += 1) {}
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +36,11 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
